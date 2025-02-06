6907d57c829e72e729affa1a9e9a00d95aed8606
Leonardo Ospino <leonardo.ospino.ayala@gmail.com>
2025-02-05 21:40:00 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+      <div className='container mx-auto mt-20'>
+        <h1 className='text-4xl'>
+          Seguimiento de Pacientes
+        </h1>
+      </div>
+    </>
+  )
+}
+
+export default App
