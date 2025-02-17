213fb4f6c5c48b6e6c983454ee43b13f8a735a74
Leonardo Ospino <leonardo.ospino.ayala@gmail.com>
2025-02-17 15:28:41 -0500

    Remove unused import

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-import { createJSONStorage, devtools, persist } from 'zustand/middleware';
+import { /*createJSONStorage, */devtools, persist } from 'zustand/middleware';
 import { v4 as uuid } from 'uuid';
 import { DraftPatient, Patient } from './types';
 
